2b54dbe893ad4ce4b6e0de915b3c52db95d75e75
minnn0119 <lokiminh@gmail.com>
2025-04-09 19:59:06 +0700

    fail 2

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Calculator {
     public int add(int a, int b) {
-        return 0;
+        return 1;
     }
     public int subtract(int a, int b) {
         return 0;
