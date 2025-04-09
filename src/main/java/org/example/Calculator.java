178e49f4a5a861867c8bb2ccd6da5eb0d9d3ae6e
minnn0119 <lokiminh@gmail.com>
2025-04-09 19:54:56 +0700

    fail 1 commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return 0;
+    }
+    public int subtract(int a, int b) {
+        return 0;
+    }
+}
